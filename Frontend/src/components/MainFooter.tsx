d4f7b8bea1ccf7964ce77a5638172404e06c6f00
mario-george <vortexspades@gmail.com>
2024-01-16 10:22:35 +0200

    Add MainFooter component

---
+++ Frontend/src/components/MainFooter.tsx
@@ -0,0 +1,13 @@
+const MainFooter = () => {
+  return (
+    <div className="bg-blue-800 py-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          HotelBooking
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default MainFooter;
