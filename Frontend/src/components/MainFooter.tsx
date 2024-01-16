6188167a33ad9f70e6359db11226de5eccae10e5
mario-george <vortexspades@gmail.com>
2024-01-16 10:22:46 +0200

    Update MainFooter component to include Privacy Policy and Terms of Service links

--- Frontend/src/components/MainFooter.tsx
+++ Frontend/src/components/MainFooter.tsx
@@ -1,13 +1,18 @@
 const MainFooter = () => {
-  return (
-    <div className="bg-blue-800 py-10">
-      <div className="container mx-auto flex justify-between items-center">
-        <span className="text-3xl text-white font-bold tracking-tight">
-          HotelBooking
-        </span>
+    return (
+      <div className="bg-blue-800 py-10">
+        <div className="container mx-auto flex justify-between items-center">
+          <span className="text-3xl text-white font-bold tracking-tight">
+            HotelBooking
+          </span>
+          <span className="text-white font-bold tracking-tight flex gap-4">
+            <p className="cursor-pointer">Privacy Policy</p>
+            <p className="cursor-pointer">Terms of Service</p>
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default MainFooter;
+    );
+  };
+  
+  export default MainFooter;
+  
