ad85fb568650af7b1fe5dc8efd2996f95ba1cf7d
mario-george <vortexspades@gmail.com>
2024-01-16 10:21:04 +0200

    Add links to My Bookings and My Hotels in MainHeader component

--- Frontend/src/components/MainHeader.tsx
+++ Frontend/src/components/MainHeader.tsx
@@ -8,7 +8,20 @@ const MainHeader = () => {
           <Link to="/">HotelBooking</Link>
         </span>
         <span className="flex space-x-2">
-      
+          <>
+            <Link
+              className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+              to="/my-bookings"
+            >
+              My Bookings
+            </Link>
+            <Link
+              className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+              to="/my-hotels"
+            >
+              My Hotels
+            </Link>
+          </>
 
           <Link
             to="/login"
