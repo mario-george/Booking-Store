ecb35f16d00e41ff0e7a7003fb668cac3092fed8
mario-george <vortexspades@gmail.com>
2024-01-16 10:34:33 +0200

    Add routing and layout components to App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
-
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+        
+        </Routes>
+      </BrowserRouter>{" "}
+    </>
+  );
 }
 
 export default App;
